6970571c95c1ea9810e30037d9b5e2b452a55c7e
Zhukov Misha <46239008+blablablajka@users.noreply.github.com>
2020-04-03 21:29:28 +0300

    Update считываниеданныхчерезпотокиввектор.cpp

--- считываниеданныхчерезпотокиввектор.cpp
+++ считываниеданныхчерезпотокиввектор.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    //////////////////////////////////
+    string number;
+    for (std::string s; number.size() < 1000 && cin >> s; number += s);  // считывание в строку строк длинной не более 1000 букв
+   ////////////////////////////////////
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (auto& it : v)
+    vector<int> v(n);   
+    for (auto& it : v) // считывание данных в вектор
         cin >> it;
-    for(auto& i:v)
-    {
+    for(auto& i:v)     // вывод данных из вектора
         cout<<i;
-    }
 }
