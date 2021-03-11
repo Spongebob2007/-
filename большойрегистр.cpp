6685306e4668ace21ff2a5c3f3a91e1f84c24d2e
OBLADAET <46239008+blablajka@users.noreply.github.com>
2021-03-11 14:26:23 +0300

    Create большойрегистр.cpp

---
+++ большойрегистр.cpp
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+  std::string str;
+  cin>>str;
+  std::transform(str.begin(), str.end(), str.begin(), (int (*) (int)) toupper);
+  cout<<str;
+}
+/*
+Real Solution
+#include <iostream>
+ 
+int main()
+{
+char chLetter('a');
+  
+std::cout << "Введите букву нижнего регистра:\n\n";
+ 
+std::cin >> chLetter;
+ 
+chLetter = chLetter - 32;
+ 
+std::cout << "\nТа же буква в верхнем регистре: " << chLetter << ".\n\n";
+ 
+return 0;
+}
+*/
